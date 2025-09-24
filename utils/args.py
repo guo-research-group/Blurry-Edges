f180f56feb24c6c2f83590db1570979de48a8e29
Wei Xu <33918092+xuweiwen@users.noreply.github.com>
2025-09-24 12:29:22 -0400

    Modified argument types for parser

--- utils/args.py
+++ utils/args.py
@@ -6,8 +6,8 @@ def get_args(mode, big=False):
     # basic settings
     parser.add_argument('--cuda', type=str, default='cuda:0', help='enable cuda')
     parser.add_argument('--model_path', type=str, default='./pretrained_weights', help='path of model weights')
-    parser.add_argument('--img_size', type=int, default=(147,147), help='image size')
-    parser.add_argument('--big_img_size', type=int, default=(587,587), help='big image size')
+    parser.add_argument('--img_size', type=int, nargs=2, default=[147,147], help='image size')
+    parser.add_argument('--big_img_size', type=int, nargs=2, default=[587,587], help='big image size')
     parser.add_argument('--R', type=int, default=21, help='patch size')
     parser.add_argument('--w', type=float, default=1, help='w in loss function')
     parser.add_argument('--alpha_lambda', type=float, default=5e-3, help='rate for lambda in ridge regression')
@@ -19,9 +19,9 @@ def get_args(mode, big=False):
         parser.add_argument('--data_path', type=str, default='./data/data_train_val', help='path of dataset')
         parser.add_argument('--num_sample_train', type=int, default=8000, help='number of samples to generate in training set')
         parser.add_argument('--num_sample_val', type=int, default=2000, help='number of samples to generate in validation set')
-        parser.add_argument('--num_shape', type=tuple, default=(15,26), help='number of shapes to generate in each image')
-        parser.add_argument('--Z_range', type=tuple, default=(0.75,1.18), help='distance between the object and the camera (m)')
-        parser.add_argument('--alpha', type=tuple, default=(180,200), help='maximum average number of photons')
+        parser.add_argument('--num_shape', type=int, nargs=2, default=[15,26], help='number of shapes to generate in each image')
+        parser.add_argument('--Z_range', type=float, nargs=2, default=[0.75,1.18], help='distance between the object and the camera (m)')
+        parser.add_argument('--alpha', type=int, nargs=2, default=[180,200], help='maximum average number of photons')
         parser.add_argument('--sigma', type=float, default=2, help='read noise coefficient')
 
     # local training
@@ -33,7 +33,7 @@ def get_args(mode, big=False):
         parser.add_argument('--batch_size', type=int, default=64, help='batch size') # 64
         parser.add_argument('--beta_bndry_loc', type=float, default=0.001, help='weight for boundary localization error')
         parser.add_argument('--beta_smthns', type=float, default=0.0005, help='weight for smoothness error')
-        parser.add_argument('--dynamic_epoch', type=float, default=200, help='key epoch of dynamic scheduling')
+        parser.add_argument('--dynamic_epoch', type=int, default=200, help='key epoch of dynamic scheduling')
     
     # global data pre-calculation
     elif mode == 'global_pre':
@@ -50,15 +50,15 @@ def get_args(mode, big=False):
         parser.add_argument('--learning_rate', type=float, default=1e-4, help='initial learning rate for late training')
         parser.add_argument('--batch_size', type=int, default=8, help='number of batch size')
 
-        parser.add_argument('--gamma_color', type=tuple, default=(1.0,0.1,0.1), help='weight for color error')
-        parser.add_argument('--gamma_color_cons', type=tuple, default=(0.2,0.1,0.05), help='weight for color consistency error')
-        parser.add_argument('--gamma_bndry_cons', type=tuple, default=(0.05,0.05,0.02), help='weight for boundary consistency error')
-        parser.add_argument('--gamma_smthns', type=tuple, default=(0.005,0.1,0.002), help='weight for smothness error')
-        parser.add_argument('--gamma_smthns_cons', type=tuple, default=(0.005,0.1,0.002), help='weight for smothness consistency error')
-        parser.add_argument('--gamma_bndry_loc', type=tuple, default=(0.0001,0.05,0.0001), help='weight for boundary localization error')
-        parser.add_argument('--gamma_depth', type=tuple, default=(0.0001,0.05,0.5), help='weight for depth error')
+        parser.add_argument('--gamma_color', type=float, nargs=3, default=[1.0,0.1,0.1], help='weight for color error')
+        parser.add_argument('--gamma_color_cons', type=float, nargs=3, default=[0.2,0.1,0.05], help='weight for color consistency error')
+        parser.add_argument('--gamma_bndry_cons', type=float, nargs=3, default=[0.05,0.05,0.02], help='weight for boundary consistency error')
+        parser.add_argument('--gamma_smthns', type=float, nargs=3, default=[0.005,0.1,0.002], help='weight for smothness error')
+        parser.add_argument('--gamma_smthns_cons', type=float, nargs=3, default=[0.005,0.1,0.002], help='weight for smothness consistency error')
+        parser.add_argument('--gamma_bndry_loc', type=float, nargs=3, default=[0.0001,0.05,0.0001], help='weight for boundary localization error')
+        parser.add_argument('--gamma_depth', type=float, nargs=3, default=[0.0001,0.05,0.5], help='weight for depth error')
 
-        parser.add_argument('--dynamic_epoch', type=tuple, default=(30,100,200), help='key epoch of dynamic scheduling')
+        parser.add_argument('--dynamic_epoch', type=int, nargs=3, default=[30,100,200], help='key epoch of dynamic scheduling')
         parser.add_argument('--input_size', type=int, default=38, help='input layer size')
         parser.add_argument('--output_size', type=int, default=12, help='output layer size')
 
@@ -68,8 +68,8 @@ def get_args(mode, big=False):
         parser.add_argument('--frgd_path', type=str, default='./data/MS_COCO_annotations/', help='path of MS COCO dataset')
         parser.add_argument('--bkgd_path', type=str, default='./data/Painting/', help='path of painting dataset')
         parser.add_argument('--num_sample_test', type=int, default=200, help='number of samples to generate in testing set')
-        parser.add_argument('--Z_range', type=tuple, default=(0.75,1.18), help='distance between the object and the camera (m)')
-        parser.add_argument('--alpha', type=tuple, default=(180,200), help='maximum average number of photons')
+        parser.add_argument('--Z_range', type=float, nargs=2, default=[0.75,1.18], help='distance between the object and the camera (m)')
+        parser.add_argument('--alpha', type=int, nargs=2, default=[180,200], help='maximum average number of photons')
         parser.add_argument('--sigma', type=float, default=2, help='read noise coefficient')
 
     # evaluation
